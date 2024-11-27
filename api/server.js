5574369129561d98254b767b053111ed9ec23668
Simeon Mechkov <montishak@gmail.com>
2024-11-28 01:19:39 +0200

    Vercel configuration

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const port = 3010;
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "../public")));
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../views"));
 
 app.get("/", async (req, res) => {
     const userIpAddress =
